7c8af53fe8557187d514a6871c7a56e77dcf17e5
El pepe <santiagonano13@gmail.com>
2025-01-05 23:52:46 -0600

    vista app

--- miapp/views.py
+++ miapp/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+# MVC = Modelo Vista Controlador --> Acciones (Metodos)
+# MVT = Modelo Template Vista    --. Acciones (Metodos)
+
+def hola_mundo(request):
+    return HttpResponse("Hola mundo con Django")
